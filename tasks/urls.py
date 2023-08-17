a8530d3ec4bbf9e5677fa7829b506d471c771404
Md. Abu Bakkar Siddiq <95091927+abu-bakkar-sebpo@users.noreply.github.com>
2023-08-17 12:50:57 +0600

    complete edit, delete, complete, repriotise task

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from tasks.views import home,add_task,view_task,completed_task,edit_task
+from tasks.views import home,add_task,show_tasks,completed_task,edit_task,delete_task,complete_task,repriotise_task
 urlpatterns = [
     path('',home,name="home"),
     path('add_task/',add_task,name="add_task"),
-    path('view_task/',view_task,name="view_task"),
+    path('show_tasks/',show_tasks,name="show_tasks"),
     path('completed_task/',completed_task,name="completed_task"),
     path('edit_task/<int:id>',edit_task,name="edit_task"),
+    path('delete_task/<int:id>',delete_task,name="delete_task"),
+    path('complete_task/<int:id>',complete_task,name="complete_task"),
+    path('repriotise_task/<int:id>',repriotise_task,name="repriotise_task"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render,redirect
 from tasks.forms import TaskForm
 from tasks.models import TaskModel
+from django.shortcuts import get_object_or_404
+
 # Create your views here.
 def home(request):
-    return render(request,"view_task.html")
+    task = TaskModel.objects.all()
+    return render(request,"show_tasks.html",{"tasks":task})
+
 def add_task(request):
     task = TaskForm(request.POST)
     if task.is_valid():
         task.save()
         print(task.cleaned_data)
-        return redirect('view_task')
+        return redirect('show_tasks')
     else:
         task =  TaskForm()
         return render(request,"add_task.html",{"form":task})
@@ -21,12 +25,29 @@ def edit_task(request,id):
         form = TaskForm(request.POST,instance=task)
         if form.is_valid():
             form.save()
-            return redirect('view_task')
+            return redirect('show_tasks')
     return render(request,'add_task.html',{'form':form})
 
-def view_task(request):
+def delete_task(request,id):
+    task = TaskModel.objects.get(pk=id).delete()
+    return redirect('show_tasks')
+
+def show_tasks(request):
     task = TaskModel.objects.all()
-    return render(request,"view_task.html",{"tasks":task})
+    return render(request,"show_tasks.html",{"tasks":task})
+
 def completed_task(request):
     task = TaskModel.objects.all()
     return render(request,"complete_task.html",{"tasks":task})
+
+def complete_task(request, id):
+    task  = TaskModel.objects.get(pk=id)
+    task.is_completed = True
+    task.save()
+    return redirect('completed_task')
+
+def repriotise_task(request, id):
+    task  = TaskModel.objects.get(pk=id)
+    task.is_completed = False
+    task.save()
+    return redirect('show_tasks')
